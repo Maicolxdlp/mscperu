53dbf7f617010e3324e9afa4e71a2b8f93bf0453
Maicolxdlp <33199743+Maicolxdlp@users.noreply.github.com>
2022-02-26 11:03:51 -0500

    Add files via upload

--- Modulos/open.py
+++ Modulos/open.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# SSHPLUS By @Crazy_vpn
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
@@ -13,9 +12,9 @@ except:
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
-MSG = 'SSHPLUS'
+MSG = 'MSCPERU'
 DEFAULT_HOST = '0.0.0.0:1194'
-RESPONSE = "HTTP/1.1 200 " + str(MSG) + "\r\n\r\n"
+RESPONSE = "HTTP/1.1 101 " + str(MSG) + "\r\n\r\n"
 
 class Server(threading.Thread):
     def __init__(self, host, port):
